e691715509d2462ef0d3a5dc61b653936b693d9b
ghostly gossip <119223255+ghostlygossip@users.noreply.github.com>
2024-02-13 10:45:49 +0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+    Welcome <?php echo $_POST["name"]; ?><break>
+    Your Roll <?php echo $_POST["roll"]; ?><break>
+    Your Semester <?php echo $_POST["semester"]; ?><break>
+    Your shift <?php echo $_POST["shift"]; ?><break>
+    Your Session <?php echo $_POST["session"]; ?><break>
+    
+</body>
+</html>
